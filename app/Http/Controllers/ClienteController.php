a70051f153b6fa141b782a114b29566556e44a48
AngeloDallacorteEuzebio <imprimir.imbe@gmail.com>
2024-10-17 06:36:33 -0300

    first commit

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cliente;
+use App\Responses\ApiResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ClienteController extends Controller
+{
+    public function salvar(Request $request)
+    {
+       
+        $validator = Validator::make($request->all(), [
+            'nome' => 'required|string|max:255',
+            'cpf' => 'required|string|max:15',
+        ]);
+    
+        if ($validator->fails()) {
+            return ApiResponse::error($validator->errors(), 'Validation error');
+        }
+    
+        $customer = Cliente::create($request->all());
+        return ApiResponse::ok('Cliente salvo com sucesso', $customer);
+       
+    }
+
+  
+
+    public function excluir(int $id)
+    {
+        $customer = Cliente::findOrFail($id);
+        $customer->delete();  
+        return ApiResponse::ok('Cliente removido com sucesso');      
+    }
+
+    public function editar(Request $request, int $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'nome' => 'required|string|max:255',
+            'cpf' => 'required|string|max:15',
+        ]);
+    
+        if ($validator->fails()) {
+            return ApiResponse::error($validator->errors(), 'Validation error');
+        }
+        $customer = Cliente::findOrFail($id);
+        $customer->update($request->all());
+        return ApiResponse::ok('Cliente atualizado com sucesso', $customer); 
+    }  
+
+    public function listar()
+    {
+        $customers = Cliente::all();
+        return ApiResponse::ok('Lista de Clientes', $customers);    
+    }
+
+    public function listarPeloID(int $id)
+    {
+        $customer = Cliente::findOrFail($id);
+        return ApiResponse::ok('Cliente a ser listado.', $customer);  
+    } 
+}
+
